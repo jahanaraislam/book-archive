5a38546da75e5356568215358b95d4d62c5a8b10
jahanaraislam <jahanaraislamniha@gmail.com>
2021-09-02 18:49:20 +0600

    second-last-commit

--- js/script.js
+++ js/script.js
@@ -6,19 +6,18 @@ const searchBook = () => {
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value
 //    clear data
-    searchText.value = ''
+    searchField.value = ''
     // handle empty search result
     if(searchText == ''){
         displayError()
     }
     else{
          // Display Spinner
-         document.getElementById('spinner').style.display = 'block';
+         spinner.style.display = 'block';
          // Hide error
-         document.getElementById('error-message').style.display = 'none';
+         errorMsg.style.display = 'none';
          // Clear Search Result
          document.getElementById('search-result').textContent ='';
-         // load data
          const url =`https://openlibrary.org/search.json?q=${searchText} `;
          console.log(url);
          fetch(url)
@@ -38,15 +37,15 @@ const displaySearchResult = data => {
     searchResult.textContent = '';
     const bookList = data.docs;
     console.log(bookList);
-    if (bookList == nul
+    if (bookList == null
         ) {
         displayError();
     }
     else {
         errorMsg.style.display = 'none';
         spinner.style.display = 'none';
-        document.getElementById('book-numbers').innerText ='Books Found ${bookList.length}';
-        //showing each book in a card
+        document.getElementById('book-numbers').innerText = `Books Found ${bookList.length}`;
+        //display book
         bookList.forEach(book => {
             const div = document.createElement('div');
             div.classList.add('col');
@@ -54,13 +53,13 @@ const displaySearchResult = data => {
             <div  class="card h-100 text-center">
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="w-75 h-50 rounded  p-2 mx-auto" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text"><b>Author</b>${book.author_name}</p>
-                    <p class="card-text"><b>Publish Year</b>: ${book.first_publish_year}</p>
-                    <p class="card-text"><b>Publisher</b>: ${book.publisher}</p>
+                    <h5 class="card-title text-center">${book.title}</h5>
+                    <p class="card-text"><b>Author</b> : ${book.author_name}</p>
+                    <p class="card-text"><b>First Publish Year</b>: ${book.first_publish_year}</p>
+                    <p class="card-text"><b>Publisher</b> : ${book.publisher}</p>
                 </div>
-                <div class="card-footer rounded text-center read">
-                <small >Read</small>
+                <div class="card-footer bg-secondary text-white rounded text-center read">
+                <small >Buy now</small>
               </div>
             </div>
             `;
